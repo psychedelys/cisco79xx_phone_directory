5a356e9f6d6d28cccdb4dbfb1027187127489544
psychedelys <psychedelys@googlemail.com>
2012-03-13 16:19:34 +0100

    The deamon is using a CSV format as input and not anymore the gdata format

--- cisco79xx_phone_directory.py
+++ cisco79xx_phone_directory.py
@@ -22,26 +22,18 @@ import re
 import unicodedata
 from flask import Flask, request
 from xml.etree import ElementTree
+import csv
 
 # CONFIGURATION REQUIRED:
 # Change these to fit your needs, the port has to match the one on
 # which the webserver listens to. It is required to hardcode the port
 # number in order to work around a bug with the Cisco 79xx browser.
-CONTACTS_FILE = "contacts.xml"
+CONTACTS_FILE = "contacts_extract.csv"
 PORT = 5006
+HOST = "192.168.178.20"
+DIRECTORY_URL = "http://%s:%d/directory.xml" % (HOST, PORT)
 
 app = Flask(__name__)
-DIGITS_RE = re.compile(r'[^\d]+')
-
-
-def remove_accents(str):
-    """
-    Thanks to MiniQuark:
-    http://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string/517974#517974
-    """
-    nkfd_form = unicodedata.normalize('NFKD', unicode(str))
-    return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
-
 
 class DirectoryEntry:
     """
@@ -50,10 +42,9 @@ class DirectoryEntry:
     """
     def __init__(self, number, name, label):
         # Discard non-digit characters
-        self.number = DIGITS_RE.sub('', number)
-        self.name = remove_accents(unicode(name))
-        # The label may contain schema information, discard it
-        self.label = remove_accents(unicode(label.replace('http://schemas.google.com/g/2005#', '')))
+        self.number = number
+        self.name = name
+        self.label = label
 
     def __str__(self):
         if self.label:
@@ -67,27 +58,23 @@ def get_directory():
     Parses the Google Contacts file and returns its contents as a list
     of DirectoryEntry instances.
     """
-    with file(CONTACTS_FILE) as f:
-        root = ElementTree.parse(f).getroot()
+    print "parsing file %s" % CONTACTS_FILE 
     directory = []
-    for children in root.findall('{http://www.w3.org/2005/Atom}entry'):
-        # Create a new entry for every phone number that belongs to the same person,
-        # which also filters out the persons that do not have phone numbers
-        phonenumbers = children.findall('{http://schemas.google.com/g/2005}phoneNumber')
-        for phone in phonenumbers:
-            number = phone.text
-            name = children.find('{http://www.w3.org/2005/Atom}title').text
-            # Tests showed that the label can be an attribute named either rel or label
-            label = phone.attrib.get('rel', phone.attrib.get('label', ''))
-            directory.append(DirectoryEntry(number, name, label))
+
+    for line in csv.reader(open(CONTACTS_FILE), delimiter=';', skipinitialspace=True):
+        # print "added: '%s' '%s' '%s'" % (line[2],line[0],line[1])
+        # directory.append(DirectoryEntry(number, name, label))
+        directory.append(DirectoryEntry(line[2],line[0],line[1]))
+
     return directory
 
 
-def generate_directory_xml(directory):
+def generate_directory_xml(directory,offset,maxEntry):
     """
     Generates the XML required to display the phone directory from
     the list of DirectoryEntry instances given as a parameter.
     """
+    # Todo: generate the next page with offset
     xml = "<CiscoIPPhoneDirectory>\n"
     xml += "\t<Title>Phone directory</Title>\n"
     xml += "\t<Prompt>Select an entry.</Prompt>\n"
@@ -112,7 +99,8 @@ def generate_search_xml():
     # device/browser for which the request.environ["SERVER_PORT"] value is
     # set to 80 although the URL accessed is on another port, therefore
     # forcing us to use a hardcoded port number
-    xml += "\t<URL>http://%s:%d/directory.xml</URL>\n" % (request.environ["SERVER_NAME"], PORT,)
+    # xml += "\t<URL>http://%s:%d/directory.xml</URL>\n" % (request.environ["SERVER_NAME"], PORT,)
+    xml += "\t<URL>%s</URL>\n" % (DIRECTORY_URL)
     xml += "\t<InputItem>\n"
     xml += "\t\t<DisplayName>Keyword</DisplayName>\n"
     xml += "\t\t<QueryStringParam>keyword</QueryStringParam>\n"
@@ -128,16 +116,29 @@ def index():
     """
     Serves the phone directory search page and the search results.
     """
+
+    if "offset" in request.args:
+        offset = request.args["offset"]
+    else:
+        offset = 0
+    maxEntry = offset + 32
+    myentry = 0
+
     # We have received the query string, display the results
     if "keyword" in request.args:
         keyword = request.args["keyword"]
         # Get the directory and filter the entries based on the keyword, then sort them
         directory = sorted([entry for entry in get_directory() if keyword.lower() in unicode(entry.name).lower() or keyword in unicode(entry.number)], key=lambda entry: unicode(entry))
-        xml = generate_directory_xml(directory)
+        xml = generate_directory_xml(directory,offset,maxEntry)
     # If we haven't received the query string, display the search menu
     else:
         xml = generate_search_xml()
     response = app.response_class(xml, mimetype='text/xml')
+    # response.headers['X-offset'] = 'parachutes are cool'
+    if myentry == 32:
+       response.headers['Refresh'] = 0
+       response.headers['url'] = "%s?offset=maxEntry" % ( DIRECTORY_URL )
+
     return response
 
 
@@ -148,5 +149,5 @@ if __name__ == "__main__":
     supported by the Cisco 79xx IP phones. Have a look at the README file
     if you haven't already!
     """
-    app.run(debug=True)
+    app.run(debug=False,port=5000)
 
